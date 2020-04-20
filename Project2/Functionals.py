46803da328ffcd50781ac387d32fe6aab113227f
chronican <55035344+chronican@users.noreply.github.com>
2020-04-20 22:35:27 +0800

    Add files via upload

--- Project2/Functionals.py
+++ Project2/Functionals.py
@@ -2,30 +2,37 @@ import _ini_
 import torch
 
 
-class MSELoss(object):
+class Relu(object):
     def __init__(self):
-        super(MSELoss, self).__init__()
+        super(Relu, self).__init__()
 
-    def loss(self, y, y_pred):
-        y = y.view(y_pred.size())
-        loss = (y - y_pred).pow(2).sum()
-        return loss
+    def func(self, x):
+        return torch.relu(x)
 
-    def grad(self, y, y_pred):
-        y = y.view(y_pred.size())
-        grad = 2 * (y - y_pred)
-        return grad
+    def grad(self, x):
+        inter = torch.max(x, torch.zeros_like(x))
+        inter[inter <= 0.] = 0.
+        inter[inter > 0.] = 1.
+        return x * inter
 
 
-class BCELoss(object):
+class Sigmoid(object):
     def __init__(self):
-        super(BCELoss, self).__init__()
+        super(Sigmoid, self).__init__()
 
-    def loss(self, y, y_pred):
-        y = y.view(y_pred.size())
-        loss = -y_pred * torch.log(y) - (1 - y_pred) * torch.log(1 - torch.sigmoid(y))
-        return loss
+    def func(self, x):
+        return (1.0 / (1 + (-x).exp())).float()
 
-    def grad(self, y, y_pred):
-        grad = -y_pred / y - (1 - y_pred) * (-1 / (1 - torch.sigmoid(y))) * (torch.exp(-y) / (1 + torch.exp(-y)) ** 2)
-        return grad
+    def grad(self, x):
+        return ((-x).exp() / (1 + (-x).exp().pow(2))).float()
+
+
+class Tanh(object):
+    def __init__(self):
+        super(Tanh, self).__init__()
+
+    def func(self, x):
+        return (x.exp() - (-x).exp()) / (x.exp() + (-x).exp()).float()
+
+    def grad(self, x):
+        return 1 / x.cosh().pow(2).float()
--- Project2/Optimizers.py
+++ Project2/Optimizers.py
@@ -1,4 +1,16 @@
-###
+import _ini_
+class SGD(object):
+    def __init__(self, learning_rate):
+        super(SGD, self).__init__()
+        self.learning_rate = learning_rate
 
-class Optimizer(object):
+    def step(self, model):
+        for k, layer in enumerate(model.layers):
+            layer.weights = layer.weights - self.learning_rate * layer.weights.grad
+            layer.bias= layer.bias - self.learning_rate * layer.bias.grad
+
+    def zero_grad(self, model):
+        self.weights.grad.zero_()
+        self.bias.grad.zero_()
+
 
